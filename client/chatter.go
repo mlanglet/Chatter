5d8b08ca9dc602477f7d4817e3de45d7f3503f6d
Mathias Langlet <mlanglet@kth.se>
2016-03-09 23:42:45 +0100

    Add

---
+++ client/chatter.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+
+	var quit string = "/quit"
+	var newLine = '\n'
+	var carrigeReturn = '\r'
+	var cleanText []byte
+	quit += string(carrigeReturn) + string(newLine)
+	fmt.Println(quit)
+	for {
+		text, err := reader.ReadBytes('\n')
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		cleanText = formatInputByOS(text)
+		sendMessage(cleanText)
+	}
+}
+
+func sendMessage([]byte) error {
+
+}
+
+func formatInputByOS(b []byte) (a []byte) {
+	// Windows input detected
+	if b[len(b)-2] == byte('\r') {
+		a = make([]byte, len(b)-2)
+	} else { // UNIX input detected
+		a = make([]byte, len(b)-1)
+	}
+
+	copy(a, b)
+
+	return
+}
+
+func printByteSlice(s []byte) {
+	for i := 0; i < len(s); i++ {
+		fmt.Fprintf(os.Stdout, "%b ", s[i])
+	}
+	fmt.Fprintln(os.Stdout, "")
+}
